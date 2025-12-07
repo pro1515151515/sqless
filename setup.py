5e23d0a7756d2918d46999bb82f7276c003eebcc
pro1515151515 <928975460@qq.com>
2025-12-07 16:07:25 +0800

    update version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="sqless",
-    version="0.1.9",
+    version="0.2.0",
     author="pro1515151515",
     author_email="pro1515151515@qq.com",
     description="An async HTTP server for SQLite, FileStorage and WebPage.",
